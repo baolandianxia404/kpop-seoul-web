d22df43a21325eb29c7d66196b6661593cfa4e0a
李雅婷 <baolandianxia404@gmail.com>
2026-07-27 19:13:11 +0800

    Revert to ArcGIS tiles (zoom fix already resolved, OSM blocked in China)

--- src/components/map/KpopMap.tsx
+++ src/components/map/KpopMap.tsx
@@ -177,8 +177,8 @@ export default function KpopMap({ locations, flyToLocation }: Props) {
         style={{ width: "100%", height: "100%" }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://www.esri.com/">Esri</a>'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
         />
 
         <MapEvents onMoveEnd={handleViewportChange} />
