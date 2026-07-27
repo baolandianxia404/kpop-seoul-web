8220ab6a5c3ef3f6a508de64d9dc210e3f62bfcc
李雅婷 <baolandianxia404@gmail.com>
2026-07-27 19:22:02 +0800

    Limit map maxZoom to 18 to prevent gray tiles at high zoom

--- src/components/map/KpopMap.tsx
+++ src/components/map/KpopMap.tsx
@@ -179,6 +179,8 @@ export default function KpopMap({ locations, flyToLocation }: Props) {
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://www.esri.com/">Esri</a>'
           url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
+          maxZoom={18}
+          maxNativeZoom={18}
         />
 
         <MapEvents onMoveEnd={handleViewportChange} />
